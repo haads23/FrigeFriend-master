beb46ad979d5b83f0a9ab6227309e24eb0112eac
haads23 <32466548+haads23@users.noreply.github.com>
2018-06-29 12:32:13 -0400

    test

--- app/src/main/java/com/example/jlee/frigefriend/LoginActivity.java
+++ app/src/main/java/com/example/jlee/frigefriend/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); //tests
 
         TextView signupTextView = (TextView) findViewById(R.id.textViewSignup);
         signupTextView.setOnClickListener(new View.OnClickListener(){
